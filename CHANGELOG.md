a349604f321382954bf84d2b4c1b1e0077fb5a0a
muah003 <muah003@gmail.com>
2019-07-09 00:33:50 +0200

    0.0.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,3 +2,7 @@
 
 ### 0.0.1
 - init
+
+### 0.0.2
+- add command to easily chose
+- make it more performant
--- src/extension.ts
+++ src/extension.ts
@@ -2,9 +2,12 @@
 
 import {
     window,
+    commands,
     workspace,
     ExtensionContext
 } from 'vscode';
+
+let themes
 
 /**
  * get settings
@@ -16,14 +19,13 @@ function getSettings() {
 }
 
 /**
- * read theme files and get scheme colors
+ * get scheme
  *
  * @param   {[type]}  style  [style description]
  *
  * @return  {[type]}         [return description]
  */
 function getScheme(style) {
-    let themes = require('./../assets/themes.json')
     let scheme = themes.filter((item) => item.name == style)
 
     if (scheme.length) {
@@ -44,31 +46,70 @@ function updateTerminalScheme(style) {
     let data
 
     if (style == 'Non') {
-        data = Object.keys(current).reduce((object, key) => {
-            if (!key.includes('terminal')) {
-                object[key] = current[key]
-            }
-            return object
-        }, {})
+        data = clearOldStyles(current)
     } else {
-        data = Object.assign(current, getScheme(style))
+        data = clearOldStyles(current)
+        data = Object.assign(data, getScheme(style))
     }
 
-    return workspace.getConfiguration().update('workbench.colorCustomizations', data, true)
+    return updateConfig('workbench.colorCustomizations', data)
+}
+
+/**
+ * update user config
+ *
+ * @param   {[type]}  key   [key description]
+ * @param   {[type]}  data  [data description]
+ *
+ * @return  {[type]}        [return description]
+ */
+function updateConfig(key, data) {
+    return workspace.getConfiguration().update(key, data, true)
         .then(
             () => true,
             (reason) => window.showErrorMessage(reason)
         );
 }
 
+/**
+ * clear old styles from user settings
+ *
+ * @param   {[type]}  list  [list description]
+ *
+ * @return  {[type]}        [return description]
+ */
+function clearOldStyles(list) {
+    return Object.keys(list).reduce((object, key) => {
+        if (!key.includes('terminal')) {
+            object[key] = list[key]
+        }
+        return object
+    }, {})
+}
+
 export function activate(context: ExtensionContext) {
+    themes = require('./../assets/themes.json')
     const settings = getSettings()
     let enabled = settings.enable
 
+    // quick pick menu
     if (enabled) {
-        updateTerminalScheme(settings.style)
+        context.subscriptions.push(
+            commands.registerCommand('terminal_themes.apply', async () => {
+                let items = themes.map((item) => item.name)
+
+                window.showQuickPick(items).then((selection) => {
+                    if (!selection) {
+                        return
+                    }
+
+                    return updateConfig('terminal_themes.style', selection)
+                })
+            })
+        )
     }
 
+    // auto update on change
     workspace.onDidChangeConfiguration((event) => {
         if (enabled && event.affectsConfiguration('terminal_themes.style')) {
             updateTerminalScheme(getSettings().style)
