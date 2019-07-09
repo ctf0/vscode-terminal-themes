f0d1a7a5786cefb5e1b42f30f80fb555ce9e4966
muah003 <muah003@gmail.com>
2019-07-09 20:54:10 +0200

    0.0.3

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,3 +6,6 @@
 ### 0.0.2
 - add command to easily chose
 - make it more performant
+
+### 0.0.3
+- get the extension one step closer to vscode theme selector
--- src/extension.ts
+++ src/extension.ts
@@ -9,6 +9,50 @@ import {
 
 let themes
 
+export function activate(context: ExtensionContext) {
+    themes = require('./../assets/themes.json')
+    const settings = getSettings()
+    let enabled = settings.enable
+
+    // quick pick menu
+    if (enabled) {
+        context.subscriptions.push(
+            commands.registerCommand('terminal_themes.apply', async () => {
+                let items = themes.map((item) => item.name)
+
+                window.showQuickPick(items, {
+                    placeHolder: 'Search Terminal Theme (up/down to preview)',
+                    onDidSelectItem: (selection) => {
+                        // preview
+                        updateTerminalScheme(selection)
+                    }
+                }).then((selection) => {
+                    if (!selection) {
+                        // make sure applied scheme is in-sync
+                        return updateTerminalScheme(getSettings().style)
+                    }
+
+                    // apply
+                    updateConfig(
+                        'terminal_themes.style',
+                        selection,
+                        window.showInformationMessage(`Terminal Theme: "${selection}" applied`)
+                    )
+                })
+            })
+        )
+    }
+
+    // auto update on change
+    workspace.onDidChangeConfiguration((event) => {
+        if (enabled && event.affectsConfiguration('terminal_themes.style')) {
+            updateTerminalScheme(getSettings().style)
+        }
+    })
+}
+
+export function deactivate() { }
+
 /**
  * get settings
  *
@@ -41,7 +85,7 @@ function getScheme(style) {
  *
  * @return  {[type]}         [return description]
  */
-function updateTerminalScheme(style) {
+async function updateTerminalScheme(style) {
     let current = workspace.getConfiguration().get('workbench.colorCustomizations')
     let data
 
@@ -52,7 +96,8 @@ function updateTerminalScheme(style) {
         data = Object.assign(data, getScheme(style))
     }
 
-    return updateConfig('workbench.colorCustomizations', data)
+    await updateConfig('workbench.colorCustomizations', data)
+    return
 }
 
 /**
@@ -60,13 +105,14 @@ function updateTerminalScheme(style) {
  *
  * @param   {[type]}  key   [key description]
  * @param   {[type]}  data  [data description]
+ * @param   {[type]}  msg  [data description]
  *
  * @return  {[type]}        [return description]
  */
-function updateConfig(key, data) {
+function updateConfig(key, data, msg: any = true) {
     return workspace.getConfiguration().update(key, data, true)
         .then(
-            () => true,
+            () => msg,
             (reason) => window.showErrorMessage(reason)
         );
 }
@@ -86,35 +132,3 @@ function clearOldStyles(list) {
         return object
     }, {})
 }
-
-export function activate(context: ExtensionContext) {
-    themes = require('./../assets/themes.json')
-    const settings = getSettings()
-    let enabled = settings.enable
-
-    // quick pick menu
-    if (enabled) {
-        context.subscriptions.push(
-            commands.registerCommand('terminal_themes.apply', async () => {
-                let items = themes.map((item) => item.name)
-
-                window.showQuickPick(items).then((selection) => {
-                    if (!selection) {
-                        return
-                    }
-
-                    return updateConfig('terminal_themes.style', selection)
-                })
-            })
-        )
-    }
-
-    // auto update on change
-    workspace.onDidChangeConfiguration((event) => {
-        if (enabled && event.affectsConfiguration('terminal_themes.style')) {
-            updateTerminalScheme(getSettings().style)
-        }
-    })
-}
-
-export function deactivate() { }
